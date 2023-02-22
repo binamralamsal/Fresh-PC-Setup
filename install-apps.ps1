c8631d59c2d4e14a7cc32a0b58a4bbf1b395a63f
Binamra Lamsal <61900781+binamralamsal@users.noreply.github.com>
2023-02-22 21:02:32 +0545

    Create install-apps.ps1

---
+++ install-apps.ps1
@@ -0,0 +1,22 @@
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+
+winget install CoreyButler.NVMforWindows
+winget install Mozilla.Firefox.DeveloperEdition
+winget install Google.Chrome.Dev
+winget install Microsoft.VisualStudioCode
+winget install Postman.Postman
+winget install Python.Python.3.11
+winget install JetBrains.Toolbox
+winget install GitHub.cli
+winget install Git.Git
+winget install Microsoft.PowerToys
+winget install Starship.Starship
+winget install ajeetdsouza.zoxide
+winget install Microsoft.PowerShell
+winget install Neovim.Neovim
+winget install M2Team.NanaZip.Preview
+winget add  PostgreSQL.PostgreSQL
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install bat
